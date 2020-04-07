1c4cb0725f4695e4814719305549f85d08b7634d
ezrankayamba <ezrankayamba@gmail.com>
2020-04-07 20:43:15 +0300

    Fix pagination

--- src/tables/Pagination.js
+++ src/tables/Pagination.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { IconFirst, IconLast, IconNext, IconPrev } from "./Incons";
 
 class Pagination extends Component {
